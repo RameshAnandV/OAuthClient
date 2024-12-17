558530179a620a150d7536804c2238f63333d3e3
vrameshanand <ramesh.anand@hcl.com>
2024-12-17 11:20:55 -0600

    firstcommit

---
+++ src/test/java/com/ranand/demo/oauthclient/OAuthClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranand.demo.oauthclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
